2649004a1e30760c56b32af56c00cc5f1695bd57
RushikeshMore11 <102665694+RushikeshMore11@users.noreply.github.com>
2022-08-29 14:32:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,17 @@ function myfun(){
     let bill=document.getElementById("bill").value;
     let tip=document.getElementById("tip").value;
     let person=document.getElementById("person").value;
-    if(bill==='' || tip===''||  person==='' )
-    {
-        alert("Enter Values");
-    }
-    else if(bill<0 || tip<0 || person<0)
-    {
-        alert("Enter positive values");
-
-    }
-    else if(person==0)
-    alert("Enter persons greater than '0'")
-    else{
     bill=Number(bill);
     tip=Number(tip);
     person=Number(person);
     let totalBill=((bill*tip)/100)+bill;
     
     let perPerson=(bill*(tip/100))/person;
-    document.getElementById("tipPerPerson").value="Tip per Person "+perPerson+" Rs";
+    document.getElementById("tipPerPerson").value="Tip "+perPerson+" Rs";
     
     let billPerPerson=totalBill/person;
     document.getElementById("totalBillPerPerson").value="Bill Per Head "+billPerPerson+" Rs";
     
     
     document.getElementById("TotalOutput").value="TotalBill "+totalBill+" Rs";
-        }
 }
